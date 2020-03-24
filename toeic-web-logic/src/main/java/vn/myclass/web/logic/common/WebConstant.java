d6e616a48bae3e41976b976de4fb879a0cb806e2
hieuhuy1995 <hieuhuy1995@gmail.com>
2020-03-24 22:16:06 +0700

    Hieu - 24/3/2020 - first time

---
+++ toeic-web-logic/src/main/java/vn/myclass/web/logic/common/WebConstant.java
@@ -0,0 +1,7 @@
+package vn.myclass.web.logic.common;
+
+/**
+ * Created by Asus on 3/23/2020.
+ */
+public class WebConstant {
+}
---
+++ toeic-web/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>hieu</title>
+</head>
+<body>
+<h1> dmmmm</h1>
+</body>
+</html> 
